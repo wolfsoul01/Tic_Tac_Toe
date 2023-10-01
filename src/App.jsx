4adbd6ea30ffaefdcff5fad94877c3c2ddb6ccc5
wolfsoul <alejandrof011021@gmail.com>
2023-10-01 12:58:44 -0400

    crada logica para determinar al ganador y detener el juego

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,61 @@ const Turn = {
   O: "o",
 };
 
-function Squire({ children, index, isTurn ,updateBoard }) {
+const WIN_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
-  const handelClick=()=>{
-    updateBoard(index)
-  }
+function Squire({ children, index, isTurn, updateBoard }) {
+  const handelClick = () => {
+    updateBoard(index);
+  };
 
   return (
-    <article className={`squire ${isTurn && "isTurn"}`} onClick={handelClick}>{children}</article>
+    <article className={`squire ${isTurn && "isTurn"}`} onClick={handelClick}>
+      {children}
+    </article>
   );
 }
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(Turn.X);
+  const [winner, setWinner] = useState(null);
+
+  const checkWinn = (boardCheck) => {
+    for (const combo of WIN_COMBOS) {
+      const [a, b, c] = combo;
+
+      if (
+        boardCheck[a] &&
+        boardCheck[a] === boardCheck[b] &&
+        boardCheck[a] === boardCheck[c]
+      ) {
+        return boardCheck[a];
+      }
+    }
+    return false;
+  };
 
   const updateBoard = (index) => {
+    if (board[index] || winner) return;
 
-    if(board[index]) return
-    
     const newTurn = turn === Turn.X ? Turn.O : Turn.X;
 
-    const newBoard= [...board]
-    newBoard[index]=turn
-    
-    setBoard(newBoard)
-    
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    const newWinner = checkWinn(newBoard);
+
+    if (newWinner) setWinner(newWinner);
+
+    setBoard(newBoard);
     setTurn(newTurn);
   };
   return (
@@ -43,7 +72,10 @@ function App() {
         {board.map((_, index) => {
           return (
             <>
-              <Squire key={index} updateBoard={updateBoard} index={index}> {board[index]} </Squire>
+              <Squire key={index} updateBoard={updateBoard} index={index}>
+                {" "}
+                {board[index]}{" "}
+              </Squire>
             </>
           );
         })}
@@ -53,6 +85,7 @@ function App() {
         <Squire isTurn={turn === Turn.X}>{Turn.X}</Squire>
         <Squire isTurn={turn === Turn.O}>{Turn.O}</Squire>
       </section>
+      <h1>{winner && `Gano:${winner}`}</h1>
     </main>
   );
 }
