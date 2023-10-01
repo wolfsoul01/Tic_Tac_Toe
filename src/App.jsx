1c554bd031deb901e52f8ad49fe1514e8f3580f2
wolfsoul <alejandrof011021@gmail.com>
2023-10-01 12:16:02 -0400

    creado el renderizado con map del tablreo

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
-
-function Squire({children}){
-
-  return <article className='squire'>
-
-{children}
-
-  </article>
+function Squire({ children,index }) {
+  return <article className="squire">{children}</article>;
 }
 
 function App() {
- 
+  const [board, setBoard] = useState(Array(9).fill(null));
 
-  return(
+  return (
     <main>
       <h1>Tic Tac Toe </h1>
 
-      <section className='board'>
-        <Squire>1</Squire>
-        <Squire>1</Squire>
-        <Squire>1</Squire>
-        <Squire>1</Squire>
-        <Squire>1</Squire>
-        <Squire>1</Squire>
-        <Squire>1</Squire>
-        <Squire>1</Squire>
-        <Squire>1</Squire>
+      <section className="board">
+        {board.map((item, index) => {
+          return (
+            <>
+              <Squire key={index}> {index} </Squire>
+            </>
+          );
+        })}
       </section>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
