d5fa7ba7bd7005873298c5333776b01b9fd186b6
wolfsoul <alejandrof011021@gmail.com>
2023-10-01 12:08:20 -0400

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+ 
+
+  return(
+    <main>
+      <h1>Tic Tac Toe </h1>
+    </main>
+  )
+}
+
+export default App
