ade503d3ff194f9efd0d0420a2b7d5c1b3ebc1c5
wolfsoul <alejandrof011021@gmail.com>
2023-10-01 12:34:55 -0400

    Crando el tablaero del turrno

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
-function Squire({ children,index }) {
-  return <article className="squire">{children}</article>;
+const Turn = {
+  X: "x",
+  O: "o",
+};
+
+function Squire({ children, index, isTurn }) {
+  return (
+    <article className={`squire ${isTurn && "isTurn"}`}>{children}</article>
+  );
 }
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(Turn.X);
 
+  const updateBoard = () => {
+    const newTurn = turn === Turn.X ? Turn.O : Turn.X;
+    setTurn(newTurn);
+  };
   return (
     <main>
       <h1>Tic Tac Toe </h1>
@@ -17,10 +29,15 @@ function App() {
         {board.map((item, index) => {
           return (
             <>
-              <Squire key={index}> {index} </Squire>
+              <Squire key={index + 1}> {index} </Squire>
             </>
           );
         })}
+      </section>
+
+      <section className="turn">
+        <Squire isTurn={turn === Turn.X}>{Turn.X}</Squire>
+        <Squire isTurn={turn === Turn.O}>{Turn.O}</Squire>
       </section>
     </main>
   );
