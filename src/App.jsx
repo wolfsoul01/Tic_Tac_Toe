fd79eda199018628dd127f097bb8296019fd7c00
wolfsoul <alejandrof011021@gmail.com>
2023-10-01 12:13:01 -0400

    Creado el comp Squire y los estilos

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,34 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
+
+function Squire({children}){
+
+  return <article className='squire'>
+
+{children}
+
+  </article>
+}
+
 function App() {
  
 
   return(
     <main>
       <h1>Tic Tac Toe </h1>
+
+      <section className='board'>
+        <Squire>1</Squire>
+        <Squire>1</Squire>
+        <Squire>1</Squire>
+        <Squire>1</Squire>
+        <Squire>1</Squire>
+        <Squire>1</Squire>
+        <Squire>1</Squire>
+        <Squire>1</Squire>
+        <Squire>1</Squire>
+      </section>
     </main>
   )
 }
