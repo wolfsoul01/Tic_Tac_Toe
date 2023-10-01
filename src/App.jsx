176bfad495b5f88651a502543a4661e8028be531
wolfsoul <alejandrof011021@gmail.com>
2023-10-01 12:48:49 -0400

    Creada la logica pricipal del juego

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,14 @@ const Turn = {
   O: "o",
 };
 
-function Squire({ children, index, isTurn }) {
+function Squire({ children, index, isTurn ,updateBoard }) {
+
+  const handelClick=()=>{
+    updateBoard(index)
+  }
+
   return (
-    <article className={`squire ${isTurn && "isTurn"}`}>{children}</article>
+    <article className={`squire ${isTurn && "isTurn"}`} onClick={handelClick}>{children}</article>
   );
 }
 
@@ -17,8 +22,17 @@ function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(Turn.X);
 
-  const updateBoard = () => {
+  const updateBoard = (index) => {
+
+    if(board[index]) return
+    
     const newTurn = turn === Turn.X ? Turn.O : Turn.X;
+
+    const newBoard= [...board]
+    newBoard[index]=turn
+    
+    setBoard(newBoard)
+    
     setTurn(newTurn);
   };
   return (
@@ -26,10 +40,10 @@ function App() {
       <h1>Tic Tac Toe </h1>
 
       <section className="board">
-        {board.map((item, index) => {
+        {board.map((_, index) => {
           return (
             <>
-              <Squire key={index + 1}> {index} </Squire>
+              <Squire key={index} updateBoard={updateBoard} index={index}> {board[index]} </Squire>
             </>
           );
         })}
